eef20f9d7af6d4ced302b7d99ae1783ba01961a7
anshu1612 <anshuchakrawarti26@gmail.com>
2026-04-17 17:51:25 +0530

    Fetch the genres

---
+++ src/components/GenreList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import useGenre from "../hooks/useGenre";
+
+const GenreList = () => {
+  const { genres, error, isLoading } = useGenre();
+  return (
+    <>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GenreList;
---
+++ src/hooks/useGenre.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+const useGenre = () => {
+  const [genres, setGenre] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenre(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+export default useGenre;
