806452a0c42d5a7ded9a72f62232039cbbab9f72
anshu1612 <anshuchakrawarti26@gmail.com>
2026-04-18 16:19:30 +0530

    Ship genres with the app

--- src/hooks/useGenre.js
+++ src/hooks/useGenre.js
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from "../data/gennres"
 
-const useGenre = () => useData("/genres");
+const useGenre = () => ({ data: genres, isLoading: false, error: null });
 export default useGenre;
