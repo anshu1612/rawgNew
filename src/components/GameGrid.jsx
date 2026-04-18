9e5fdac75e082a71ffd899de9ada14faaa63e75e
anshu1612 <anshuchakrawarti26@gmail.com>
2026-04-18 16:28:34 +0530

    Refactor game grid

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -4,16 +4,18 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
-const GameGrid = ({gameQuery}) => {
+const GameGrid = ({ gameQuery }) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text>{error}</Text>;
+
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
       padding="10px"
       spacing={6}
     >
-      {error && <Text>{error}</Text>}
       {isLoading &&
         skeletons.map((skeleton) => (
           <GameCardContainer key={skeleton}>
