fcac50561c2fc4062bba21c1b132cc305401b03f
anshu1612 <anshuchakrawarti26@gmail.com>
2026-04-29 22:32:45 +0530

    Build expandable text

--- src/pages/GameDetailsPage.jsx
+++ src/pages/GameDetailsPage.jsx
@@ -2,16 +2,18 @@ import { Heading, Spinner, Text } from "@chakra-ui/react";
 import React from "react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug);
   if (isLoading) return <Spinner />;
   if (error) throw error;
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
